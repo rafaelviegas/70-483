76d50d35c7a2d94622c6da811c9ad562d32fcbad
Rafael Viegas <rafael.viegas@riseupsolutions.com.br>
2016-05-15 16:53:26 -0300

    Creating a thread with the Thread class

--- Chapter1/Chapter1/Program.cs
+++ Chapter1/Chapter1/Program.cs
@@ -1,9 +1,38 @@
-﻿namespace Chapter1
+﻿using System;
+using System.Threading;
+
+namespace Chapter1
 {
     public static class Program
     {
-        static void Main()
+        //Criando método para simulação de uma thread.
+        public static void ThreadMethod()
         {
+            //A thread então imprime números de 0 a 9.
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                //Usando o método Sleep para simular a suspenção desta thread por 0 milissegundos.
+                Thread.Sleep(0);
+            }
+        }
+        public static void Main()
+        {
+            //Instanciando uma nova thread com o método que criei no inicio do programa.
+            var t = new Thread(ThreadMethod);
+            //startando a thread.
+            t.Start();
+            //Criando um processo qualquer no meu método main
+            for (var i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main thread: Do some work.");
+                //Usando o método Sleep para simular a suspenção desta thread por 0 milissegundos.
+                Thread.Sleep(0);
+            }
+            //Usando o método Join para que o programa aguarde o termino desta thread para prosseguir.
+            t.Join();
+
+            Console.ReadKey();
 
         }
     }
