6ede66c13c4f763bc1327db412a91aaf949800ad
Rafael Viegas <rafael.viegas@riseupsolutions.com.br>
2016-05-18 00:37:11 -0300

    Using the ParameterizedThreadStart

--- Chapter1/Chapter1/Program.cs
+++ Chapter1/Chapter1/Program.cs
@@ -6,10 +6,10 @@ namespace Chapter1
     public static class Program
     {
         //Criando método para simulação de uma thread.
-        public static void ThreadMethod()
+        public static void ThreadMethod(object o)
         {
             //A thread então imprime números de 0 a 9.
-            for (var i = 0; i < 10; i++)
+            for (var i = 0; i < (int)o; i++)
             {
                 Console.WriteLine("ThreadProc: {0}", i);
                 //Usando o método Sleep para simular a suspenção desta thread por 0 milissegundos.
@@ -18,12 +18,12 @@ namespace Chapter1
         }
         public static void Main()
         {
-            //Instanciando uma nova thread em background com o método que criei no inicio do programa
-            var t = new Thread(ThreadMethod) {IsBackground = true};
+            //Instanciando uma nova thread parametrizada com o método que criei no inicio do programa
+            var t = new Thread(ThreadMethod);
 
-            //startando a thread.
-            t.Start();
-  
+            //startando a thread passando um valor;
+            t.Start(5);
+            t.Join();
 
             Console.ReadKey();
 
