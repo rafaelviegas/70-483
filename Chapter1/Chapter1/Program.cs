cb647a49ebd5042f0221e425f4236e39d9508360
Rafael Viegas <rafael.viegas@faec.sp.gov.br>
2017-11-15 20:53:21 -0200

    Stopping a thread

--- Chapter1/Chapter1/Program.cs
+++ Chapter1/Chapter1/Program.cs
@@ -5,28 +5,25 @@ namespace Chapter1
 {
     public static class Program
     {
-        //Criando método para simulação de uma thread.
-        public static void ThreadMethod(object o)
-        {
-            //A thread então imprime números de 0 a 9.
-            for (var i = 0; i < (int)o; i++)
-            {
-                Console.WriteLine("ThreadProc: {0}", i);
-                //Usando o método Sleep para simular a suspenção desta thread por 0 milissegundos.
-                Thread.Sleep(1000);
-            }
-        }
         public static void Main()
         {
-            //Instanciando uma nova thread parametrizada com o método que criei no inicio do programa
-            var t = new Thread(ThreadMethod);
+            bool stopped = false;
 
-            //startando a thread passando um valor;
-            t.Start(5);
-            t.Join();
+            var t = new Thread(() =>
+            {
+                while (!stopped)
+                {
+                    Console.WriteLine("Rodando...");
+                    Thread.Sleep(1000);
+                }
+            });
 
+            t.Start();
+            Console.WriteLine("Aperte uma tecla para sair...");
             Console.ReadKey();
+            stopped = true;
 
+            t.Join();
         }
     }
 }
