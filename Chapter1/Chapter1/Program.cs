90e071cab37dddc761e51ba71ea49f43ad57c0f1
Rafael Viegas <rafael.viegas@faec.sp.gov.br>
2017-11-15 22:32:34 -0200

    Using ThreadLocal<T>

--- Chapter1/Chapter1/Program.cs
+++ Chapter1/Chapter1/Program.cs
@@ -5,29 +5,35 @@ namespace Chapter1
 {
     public static class Program
     {
-  
-        [ThreadStatic]
-        //Com o atributo ThreadStatic cada thread terá um escopo da variável _field
-        public static int _field;
+
+        /*
+         * Usando o atributo ThreadStatic a variável é inicializada apenas uma vez
+         * Com ThreadLocal<T> a variável é inicializada dentro do escopo de cada Thread.
+        */
+
+        public static ThreadLocal<int> _field = new ThreadLocal<int>(() =>
+         {
+             return Thread.CurrentThread.ManagedThreadId;
+         });
 
         public static void Main()
         {
 
             new Thread(() =>
             {
-                for (int i = 0; i < 10; i++)
+                for (int i = 0; i < _field.Value; i++)
                 {
-                    _field++;
-                    Console.WriteLine("Thread A: {0}",_field);
+                  
+                    Console.WriteLine("Thread A: {0}",i);
                 }
             }).Start();
 
             new Thread(() =>
             {
-                for (int i = 0; i < 10; i++)
+                for (int i = 0; i < _field.Value; i++)
                 {
-                    _field++;
-                    Console.WriteLine("Thread B: {0}", _field);
+               
+                    Console.WriteLine("Thread B: {0}", i);
                 }
             }).Start();
 
